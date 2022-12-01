26547f1ccfa447330587be5b6b7d42efc5608274
Mauricio Ferrato <mferrato@udel.edu>
2022-12-01 11:42:12 -0500

    added LightGBM implementation

--- classification.py
+++ classification.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
 import xgboost
+import lightgbm
 import numpy as np
 import pandas as pd
 import data_preprocess as dp
@@ -25,6 +26,16 @@ gdb_parameters = {
 'reg_lambda': [0.1, 1.0, 5.0, 10.0, 50.0, 100.0],
 'n_estimators': [100, 150, 200, 250, 500, 750, 1000]}
 
+lgbm_parameters = {
+'max_depth':[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, None],
+'learning_rate': [0.001, 0.01, 0.1, 0.2, 0.3],
+'subsample': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+'colsample_bytree': [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+'min_child_weight': [0.5, 1.0, 3.0, 5.0, 7.0, 10.0],
+'reg_lambda': [0.1, 1.0, 5.0, 10.0, 50.0, 100.0],
+'n_estimators': [100, 150, 200, 250, 500, 750, 1000]}
+
+
 # Classification wrapper used to select the correct classifier based on the configuration file selection
 def get_model(classifier, hyper_opt):
 
@@ -46,6 +57,14 @@ def get_model(classifier, hyper_opt):
         # going through the list of hyperparameters above and selects best performing model
         if hyper_opt == "random_search":
             model = RandomizedSearchCV(model, gdb_parameters, n_iter=30,
+                                            n_jobs=-1, verbose=0, cv=5,
+                                            scoring='roc_auc', refit=True, random_state=42)
+    elif classifier == 'lgbm':
+        model = lightgbm.LGBMClassifier()
+        # Random Search CV used for Hyperparameter optimization, sets up the operation for
+        # going through the list of hyperparameters above and selects best performing model
+        if hyper_opt == "random_search":
+            model = RandomizedSearchCV(model, lgbm_parameters, n_iter=30,
                                             n_jobs=-1, verbose=0, cv=5,
                                             scoring='roc_auc', refit=True, random_state=42)
     else:
