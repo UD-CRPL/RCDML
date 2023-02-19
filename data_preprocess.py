693006a3b31a9c94a452806f49142d01b4730ab8
Mauricio Ferrato <mferrato@github.com>
2023-02-18 21:16:39 -0500

    Added a way to multiply the dataset by simulating more samples

--- data_preprocess.py
+++ data_preprocess.py
@@ -156,3 +156,25 @@ def load_dataset_rnaseq(url):
 def load_labels_rnaseq(url):
     labels = pd.read_csv(url, sep='\t', index_col=0)
     return labels
+
+def simulate_data(dataset, labels, simulation_size):
+    
+    dataset_size = len(dataset.columns)
+    extra_samples_size = simulation_size - dataset_size
+    
+    if(extra_samples_size < 0): 
+        sys.exit("Requested simulation of data that's smaller than sample size, please change the simulation size")
+
+    extra_samples = labels.groupby("GROUP").sample(n = int(extra_samples_size / 2), random_state=1, replace = True)
+    
+    sampled_dataset = dataset[extra_samples["SID"]]
+    
+    col_names = ["simulated_sample_" + str(i) for i in range(0, extra_samples_size)]
+    
+    extra_samples["SID"] = col_names
+    sampled_dataset.columns = col_names
+    
+    dataset = pd.concat([dataset, sampled_dataset], axis=1)
+    labels = pd.concat([labels,  extra_samples], axis=0)
+
+    return dataset, labels, dataset.columns
